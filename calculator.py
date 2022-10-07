b61b7188cdf0f152c8e8fe5baf546666aa0a5879
nikitacbhole <70900671+nikitacbhole@users.noreply.github.com>
2022-10-07 10:36:45 +0530

    Create calculator.py
    Created the code as per the requirement in readme.md file.

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#python code for calculator
+ #1 for addition
+#2 for subtraction
+#3 for multiplication
+#4 for division
+
+operation_no = int(input('Enter the operation id: '))
+n1= float(input('Enter value: '))
+n2= float(input('Enter value: '))
+
+if (operation_no==1):
+    print('Addition Result:', n1+n2)
+
+if (operation_no==2):
+    print('Subtraction Result:', n1-n2)
+
+
+if (operation_no==3):
+    print('Multiplication Result:', n1*n2)
+
+
+if (operation_no==4):
+    print('Divisionof Result:', n1/n2)
